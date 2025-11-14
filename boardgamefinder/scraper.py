798302b37721796c3d4d33a751fc546ca4bf2c80
wouterstultiens <wouterstultiens@gmail.com>
2025-11-14 17:16:23 +0100

    .

--- boardgamefinder/scraper.py
+++ boardgamefinder/scraper.py
@@ -8,22 +8,26 @@ from .models import Listing
 
 
 def _full_description(url: str) -> str:
-    html = requests.get(
-        url,
-        timeout=20,
-        headers={
-            "User-Agent": (
-                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-                "(KHTML, like Gecko) Chrome/120.0.0 Safari/537.36"
-            )
-        },
-    ).text
+    try:
+        html = requests.get(
+            url,
+            timeout=20,
+            headers={
+                "User-Agent": (
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+                    "(KHTML, like Gecko) Chrome/120.0.0 Safari/537.36"
+                )
+            },
+        ).text
 
-    soup = BeautifulSoup(html, "html.parser")
-    desc_div = soup.select_one("div.Description-description")
-    inner = desc_div.decode_contents()
-    inner = inner.replace("<br/>", "\n").replace("<br />", "\n").replace("<br>", "\n")
-    return BeautifulSoup(inner, "html.parser").get_text()
+        soup = BeautifulSoup(html, "html.parser")
+        desc_div = soup.select_one("div.Description-description")
+        inner = desc_div.decode_contents()
+        inner = inner.replace("<br/>", "\n").replace("<br />", "\n").replace("<br>", "\n")
+        return BeautifulSoup(inner, "html.parser").get_text()
+    except:
+        print("\n\n\033[31mUNABLE TO GET FULL DESCRIPTION\033[0m\n\n")
+        return ""
 
 
 def _to_listing_data(listing) -> Listing:
@@ -32,7 +36,7 @@ def _to_listing_data(listing) -> Listing:
 
     return Listing(
         title=str(listing.title),
-        description=_full_description(url),
+        description=_full_description(url) or listing.description,
         price=float(listing.price),
         price_type=str(listing.price_type),
         link=url,
